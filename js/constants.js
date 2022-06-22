438cfa425cc2c385930ef36c6df3c94c6ec1154a
nvrrus <nikita.raevnev@gmail.com>
2022-06-22 22:03:56 +0300

    исправляет замечание

---
+++ js/constants.js
@@ -0,0 +1,14 @@
+const CONSTANTS = {
+  OFFER_TYPES: ['palace', 'flat', 'house', 'bungalow', 'hotel'],
+  TIMES: ['12:00', '13:00', '14:00'],
+  FEATURES: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+  DESCRIPTIONS: ['Big roomLocated in the city center, great view from the window, cozy rooms, delicious food',
+    'There is a cafe and a restaurant, the restaurant has good cuisine and a normal average check',
+    'Water, tea, coffee and a welcome fruit platter - pleasantly surprised by the quality'],
+  PHOTOS: ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'],
+  MAX_PRICE: 100000
+};
+
+export { CONSTANTS };
--- js/data.js
+++ js/data.js
@@ -1,15 +1,6 @@
 import { getRandomInteger, getRandomFloat, getRandomArray } from './utils.js';
+import { CONSTANTS } from './constants.js';
 
-const OFFER_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const TIMES = ['12:00', '13:00', '14:00'];
-const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const DESCRIPTIONS = ['Big roomLocated in the city center, great view from the window, cozy rooms, delicious food',
-  'There is a cafe and a restaurant, the restaurant has good cuisine and a normal average check',
-  'Water, tea, coffee and a welcome fruit platter - pleasantly surprised by the quality'];
-const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
-const MAX_PRICE = 100000;
 
 function createMocks(length) {
   return Array.from({ length: length }, () => createMock());
@@ -36,15 +27,15 @@ function createOffer(location) {
   return {
     title: `title${getRandomInteger(1, 10)}`,
     address: `${location.lat}, ${location.lng}`,
-    price: getRandomInteger(1, MAX_PRICE),
-    type: OFFER_TYPES[getRandomInteger(0, OFFER_TYPES.length - 1)],
-    rooms: getRandomInteger(1, MAX_PRICE),
-    guests: getRandomInteger(1, MAX_PRICE),
-    checkin: TIMES[getRandomInteger(0, TIMES.length - 1)],
-    checkout: TIMES[getRandomInteger(0, TIMES.length - 1)],
-    features: getRandomArray(FEATURES),
-    description: DESCRIPTIONS[getRandomInteger(0, DESCRIPTIONS.length - 1)],
-    photos: getRandomArray(PHOTOS),
+    price: getRandomInteger(1, CONSTANTS.MAX_PRICE),
+    type: CONSTANTS.OFFER_TYPES[getRandomInteger(0, CONSTANTS.OFFER_TYPES.length - 1)],
+    rooms: getRandomInteger(1, CONSTANTS.MAX_PRICE),
+    guests: getRandomInteger(1, CONSTANTS.MAX_PRICE),
+    checkin: CONSTANTS.TIMES[getRandomInteger(0, CONSTANTS.TIMES.length - 1)],
+    checkout: CONSTANTS.TIMES[getRandomInteger(0, CONSTANTS.TIMES.length - 1)],
+    features: getRandomArray(CONSTANTS.FEATURES),
+    description: CONSTANTS.DESCRIPTIONS[getRandomInteger(0, CONSTANTS.DESCRIPTIONS.length - 1)],
+    photos: getRandomArray(CONSTANTS.PHOTOS),
   };
 }
 
